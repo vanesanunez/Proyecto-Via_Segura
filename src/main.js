35b7b11f3fa36cfb2a2bb6ef3063c9423b50c9b6
tatilai <ramirezlaila.t23@gmail.com>
2025-11-02 15:18:29 -0300

    agregar configuración inicial de Vuetify con iconos Material Design

--- src/main.js
+++ src/main.js
@@ -7,4 +7,16 @@ import App from "./App.vue";
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+app.use(vuetify)
+
+//  Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css' // iconos mdi
+
+
+
+const vuetify = createVuetify({ components, directives })
 
