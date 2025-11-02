52679db5e927ffccc4fd33480a139db412a20a5d
vanesanunez <142461430+vanesanunez@users.noreply.github.com>
2025-11-02 19:01:59 -0300

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,30 +1,8 @@
-
-/*import '@mdi/font/css/materialdesignicons.css' 
-import 'vuetify/styles'*/
-
-
-
 import './style.css';
 import 'leaflet/dist/leaflet.css';
-
-//Vue y Router
 import { createApp } from "vue";
 import router from "./router/router";
 import App from "./App.vue";
-
-// Vuetify
-// import 'vuetify/styles'
-// import { createVuetify } from 'vuetify'
-// import * as components from 'vuetify/components'
-// import * as directives from 'vuetify/directives'
-
-// Components
-// import App from './App.vue'
-
-// const vuetify = createVuetify({
-//   components,
-//   directives,
-// })
 
 const app = createApp(App);
 app.use(router);
