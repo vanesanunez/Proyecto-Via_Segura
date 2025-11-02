e0efce960644408467f9ee504061e0a8463b55c6
vanesanunez <142461430+vanesanunez@users.noreply.github.com>
2025-11-02 18:53:14 -0300

    ...

--- src/main.js
+++ src/main.js
@@ -6,29 +6,28 @@ import 'vuetify/styles'*/
 
 import './style.css';
 import 'leaflet/dist/leaflet.css';
-// iconos mdi
-
-
-//vuetify primero
-
-/*import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'*/
 
 //Vue y Router
 import { createApp } from "vue";
 import router from "./router/router";
 import App from "./App.vue";
 
-//crear Vuetify y App
-//const vuetify = createVuetify({ components, directives })
+// Vuetify
+// import 'vuetify/styles'
+// import { createVuetify } from 'vuetify'
+// import * as components from 'vuetify/components'
+// import * as directives from 'vuetify/directives'
+
+// Components
+// import App from './App.vue'
+
+// const vuetify = createVuetify({
+//   components,
+//   directives,
+// })
+
 const app = createApp(App);
-
-//usar dependencias en orden
 app.use(router);
-//app.use(vuetify);
-
-//montar la app
 app.mount('#app');
 
 
